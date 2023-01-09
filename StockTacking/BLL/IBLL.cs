43cc87cdfabfa3d7e2a18f62f99262c43a11c8ab
Hernan Moyano <hernan.moyano.1989@outlook.com>
2023-01-09 16:57:42 -0300

    Agregar archivos de proyecto.

---
+++ StockTacking/BLL/IBLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTacking.BLL
+{
+    internal interface IBLL<T,K> where T: class where K : class
+    {
+        K Select();
+        bool Insert(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        bool GetBack(int ID);
+    }
+}
---
+++ StockTacking/DAL/DAO/IDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTacking.DAL.DAO
+{
+    internal interface IDAO<T,K> where T: class where K: class
+    {
+        //coleccion de objetos
+        List<K> Select();
+        //tipo de dato
+        bool Insert(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        bool GetBack(int ID);
+    }
+}
